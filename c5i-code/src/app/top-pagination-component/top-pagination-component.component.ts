10e10b765585a7c440992edd42aeb7578f97e98e
arjunhadke96 <41570479+arjunhadke96@users.noreply.github.com>
2019-06-20 14:17:08 +0530

    fixed

--- c5i-code/src/app/top-pagination-component/top-pagination-component.component.ts
+++ c5i-code/src/app/top-pagination-component/top-pagination-component.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-pagination-component',
-  templateUrl: './top-pagination-component.component.html',
-  styleUrls: ['./top-pagination-component.component.css']
+  templateUrl: './top-pagination-component.component.html'
 })
 export class TopPaginationComponentComponent implements OnInit {
 
